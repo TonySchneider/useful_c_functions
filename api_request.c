e50d9fe549ea121041c43bdec59c92861a70ecbe
tonyschneider <tonysch05@gmail.com>
2023-04-24 14:26:40 +0300

    changed the lib to json-c

--- api_request.c
+++ api_request.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 #include <string.h>
-#include <jansson.h>
+#include <json-c/json.h>
 
 // function to send an API request and check if "isVerified" is true in the response
 int is_verified(char* file_path, char* hash_value) {
@@ -10,8 +10,10 @@ int is_verified(char* file_path, char* hash_value) {
     CURLcode res;
     char url[1000];
     char response[1000];
-    json_t *root;
-    json_error_t error;
+    struct json_object *root;
+    struct json_tokener *tok;
+    enum json_tokener_error error;
+    int is_verified = 0;
 
     // create the URL for the API request
     sprintf(url, "http://example.com/verify_credential_app/%s/%s", file_path, hash_value);
@@ -38,19 +40,24 @@ int is_verified(char* file_path, char* hash_value) {
     }
 
     // parse the JSON response
-    root = json_loads(response, 0, &error);
+    tok = json_tokener_new();
+    root = json_tokener_parse_ex(tok, response, strlen(response));
 
     if(!root) {
-        fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+        fprintf(stderr, "error: %s\n", json_tokener_error_desc(tok->err));
+        json_tokener_free(tok);
         return 0;
     }
 
     // check if "isVerified" is true in the JSON response
-    json_t *is_verified_value = json_object_get(root, "isVerified");
-    int is_verified = json_is_true(is_verified_value);
+    struct json_object *is_verified_obj;
+    if (json_object_object_get_ex(root, "isVerified", &is_verified_obj)) {
+        is_verified = json_object_get_boolean(is_verified_obj);
+    }
 
     // cleanup JSON
-    json_decref(root);
+    json_object_put(root);
+    json_tokener_free(tok);
 
     // return 1 if "isVerified" is true, or 0 if it's not
     return is_verified;
